701f4151a1a41f031df07350f719cfc54c25d62d
Tomás Saieg <tomas.saieg.paez@gmail.com>
2017-07-08 17:02:55 -0400

    varios

--- bucalemu-cordova-match-images/src/index.js
+++ bucalemu-cordova-match-images/src/index.js
@@ -1,11 +1,11 @@
-var arrayBufferToBuffer = require('arraybuffer-to-buffer')
+var $ = require("jquery");
 var jimp = require('jimp')
+var arrayBufferToBuffer = require('arraybuffer-to-buffer')
 var config = require('./config')
-
 
 var jimpApp = {
   fetchFromUrl: function (imgUrl) {
-    // returns a Promise, the Promise is resolved with a jimp image object
+    // returns a Promise, the Promise is resolved with a jimp image object created from imgUrl
     return new Promise(
       function (resolve, reject) {
         fetch(imgUrl)
@@ -18,6 +18,17 @@ var jimpApp = {
           .then(function (jimpImage) { resolve(jimpImage) })
       }
     )
+  },
+
+  getSimilarity: function (jImage1, jImage2) {
+    // returns a similarity index for jImage1 and jImage2, 0 for totally different and 1 for equal
+    // for now very simple, may need to make it more complicated in the future
+    let pixelDifference = jimp.diff(jImage1, jImage2).percent  // pixel difference
+    let hammingDistance = jimp.distance(jImage1, jImage2)      // hamming distance
+
+    let similarityIndex = 1 - Math.min(pixelDifference, hammingDistance)
+
+    return similarityIndex
   },
 
   init: function () {
@@ -42,11 +53,13 @@ var app = {
   previousJImage: false,
 
   startCamera: function () {
-    let width = parseInt(window.screen.width * 0.5)
-    let height = parseInt((window.screen.width * 0.5) * 640 / 480)
-    let x = parseInt((window.screen.width - width) / 2)
-    let y = 5
-    CameraPreview.startCamera({ x: x, y: y, width: width, height: height, camera: "back", toBack: false, previewDrag: false, tapPhoto: true });
+
+    let cameraWidth = parseInt($(window).width() * 0.5 - 20);
+    let cameraHeight = parseInt(($(window).width() * 0.5) * 640 / 480);
+    let top = parseInt($(window).height() - cameraHeight - 5)
+    let left = parseInt($(window).width() - cameraWidth - 5)
+    
+    CameraPreview.startCamera({ x: left, y: top, width: cameraWidth, height: cameraHeight, camera: "back", toBack: true, previewDrag: false, tapPhoto: true });
   },
 
   sendBase64ToServer: function (base64) {
@@ -70,26 +83,30 @@ var app = {
   },
 
   uploadPicture: function () {
-    console.log("here I am")
+    console.log("status: uploading picture")
     console.log(app.previousImgData)
     app.sendBase64ToServer(app.previousImgData)
   },
 
   takePicture: function () {
+    console.log("status: taking picture")
     CameraPreview.takePicture({ width: 640, height: 480, quality: 85 }, function (imgData) {
       let imgBuffer = Buffer.from(imgData[0], 'base64')
-      
       jimp.read(imgBuffer).then(function (jImage) {
+        console.log("status: just read buffer")
         if (app.previousJImage) {
-          let diff = jimp.diff(jImage, app.previousJImage)           // pixel difference
-          let distance = jimp.distance(jImage, app.previousJImage)   // hamming distance
-          let similarity = Math.max(parseInt((1 - diff.percent) * 100), parseInt((1 - distance) * 100))
+          let similarity = parseInt(jimpApp.getSimilarity(jImage, app.previousJImage) * 100)
 
-          document.getElementById('similarityScore').innerHTML = similarity.toString() + "% similares"
-          document.getElementById('pixelDistance').innerHTML = "Pixel distance: " + diff.percent.toString()
-          document.getElementById('hammingDistance').innerHTML = "Hamming distance: " + distance.toString()
+          $("#similarityScore").html(similarity.toString() + "%<br/>similares")
+
+          if (similarity >= 80) {
+            $("#similarityScore").addClass('green').removeClass('red')
+          } else {
+            $("#similarityScore").addClass('red').removeClass('green')
+          }
         }
 
+        console.log("status: switching images")
         document.getElementById('pictureA').src = 'data:image/jpeg;base64,' + imgData[0]
         if (app.previousImgData) {
           document.getElementById('pictureB').src = 'data:image/jpeg;base64,' + app.previousImgData
@@ -109,12 +126,44 @@ var app = {
     this.startCamera()
     this.show()
 
-    document.getElementById('pictureA').width = parseInt(window.screen.width * 0.4)
-    document.getElementById('pictureA').height = parseInt((window.screen.width * 0.4) * 640 / 480)
-    document.getElementById('pictureB').width = parseInt(window.screen.width * 0.4)
-    document.getElementById('pictureB').height = parseInt((window.screen.width * 0.4) * 640 / 480)
+    let windowWidth = parseInt($(window).width())
+    let windowHeight = parseInt($(window).height())
 
-    document.getElementById('pictureDiv').style.marginTop = parseInt((window.screen.width * 0.5) * 640 / 480 + 10).toString() + "px"
+    let pictureFrameWitdh = parseInt($(window).width() * 0.5 - 20)
+    let pictureFrameHeight = parseInt(($(window).width() * 0.5) * 640 / 480)
+
+    $("#pictureB").width(pictureFrameWitdh)
+    $("#pictureB").height(pictureFrameHeight)
+    $("#pictureB").css('left', "5px")
+    $("#pictureB").css('top', "5px")
+
+    $("#pictureA").width(pictureFrameWitdh)
+    $("#pictureA").height(pictureFrameHeight)
+    $("#pictureA").css('left', "5px")
+    $("#pictureA").css('top', parseInt(windowHeight - $("#pictureA").height() - 5) + "px")
+
+    $("#takePictureButton").css('left', parseInt(windowWidth
+                                               - $("#takePictureButton").outerWidth()
+                                               - 10)
+                                       + "px")
+    $("#takePictureButton").css('top', parseInt(windowHeight
+                                              - pictureFrameHeight
+                                              + 5)
+                                       + "px")
+
+    $("#uploadButton").css('left', parseInt(pictureFrameWitdh
+                                          - $("#uploadButton").outerWidth()
+                                          - 5)
+                                   + "px")
+    $("#uploadButton").css('top', parseInt(windowHeight
+                                         - pictureFrameHeight
+                                         + 5)
+                                  + "px")
+
+    $("#similarityScoreDiv").width(pictureFrameWitdh)
+    $("#similarityScoreDiv").height(windowHeight - 2*pictureFrameHeight - 20)
+    $("#similarityScoreDiv").css('left', "5px")
+    $("#similarityScoreDiv").css('top', parseInt(pictureFrameHeight + 10) + "px")
 
     document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
     document.getElementById('uploadButton').addEventListener('click', this.uploadPicture, false);
